1b49c6285d50ad95601c8301d9626b7d2efa6442
Neerajlog <85436446+Neerajlog@users.noreply.github.com>
2022-06-09 01:23:53 +0530

    Add files via upload

---
+++ Appium-tutorial/src/test/java/appiumtests/CaculatorTest.java
@@ -0,0 +1,33 @@
+package appiumtests;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+
+public class CaculatorTest {
+
+ static	AppiumDriver<MobileElement> driver;
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+         openCaculator();
+	}
+	
+	public static void openCaculator() throws MalformedURLException {
+		DesiredCapabilities cap=new DesiredCapabilities();
+		cap.setCapability("deviceName", "realme Narzo 10A");
+		cap.setCapability("udid", "G6GYSOEUVKR85D5S");
+		cap.setCapability("plateformVersion", "11");
+	    cap.setCapability("appPackage", "com.coloros.calculator");
+	    cap.setCapability("appActivity", "com.android.calculator2.Calculator");
+	   
+	    URL url=new URL("http://127.0.0.0.0:4723/wd/hub");
+	driver=new AppiumDriver<MobileElement>(url,cap);
+	
+	}
+
+}
